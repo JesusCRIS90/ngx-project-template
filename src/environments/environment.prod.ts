c10f01cb77a8f87b1fd347b3a4e67880a7860c89
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2025-05-17 13:46:34 +0200

    [ADD] Implement environment configuration and utility functions for asset path management

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    baseHref: '/your-github-repo-name-here' // <-- Match your GitHub repo name
+};
---
+++ src/environments/index.ts
@@ -0,0 +1,1 @@
+export { getRightPath } from "./utils-functions"
