84dfc5ada70893cfef414e2d0db9d5a17a523a62
JesusCRIS90 <Jesus_CR-90@hotmail.es>
2025-05-17 13:57:11 +0200

    [UPDATE] Add bee-modal-window-host component to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { ModalWindowHostComponent } from "@beexy/ngx-modals"
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ModalWindowHostComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
